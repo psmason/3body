37f3621566b96bfa570fc7d608b25a52e82b5c96
Patrick <patrick.s.mason@gmail.com>
2018-09-28 23:14:01 -0600

    ogg format looks to work with chrome

--- play.sh
+++ play.sh
@@ -1,3 +1,3 @@
 go run lissajous.go \
-    | ffmpeg -f image2pipe -pix_fmt yuv420p -r 24 -i - -f matroska - \
+    | ffmpeg -f image2pipe -pix_fmt yuv420p -r 8 -i - -f ogg -qscale:v 10 -f ogg - \
     | ffplay -

4aff93b96ab12be643f838ca340721c17ec63da9
Patrick <patrick.s.mason@gmail.com>
2018-09-28 23:00:58 -0600

    formatting

--- play.sh
+++ play.sh
@@ -1,1 +1,3 @@
-go run lissajous.go | ffmpeg -f image2pipe -pix_fmt yuv420p -r 24 -i - -f matroska - | ffplay -
+go run lissajous.go \
+    | ffmpeg -f image2pipe -pix_fmt yuv420p -r 24 -i - -f matroska - \
+    | ffplay -
